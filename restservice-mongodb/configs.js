5771d6674cbdd9e90853f2731ac2d455cf1543c3
amtushar <tushardhingra37@gmail.com>
2025-04-23 17:53:43 +0530

    mongoDB Atlas url and backend https url

--- restservice-mongodb/configs.js
+++ restservice-mongodb/configs.js
@@ -1,10 +1,9 @@
 
 module.exports = class configs {
 
-    static REST_SERVICE_ENV = "local"
-    static REST_SERVICE_HOST = "localhost";
+    static REST_SERVICE_HOST = "localhost"; // not updated as it is being used for console only
     static REST_SERVICE_PORT = "7009"; 
 
 
-    static DATABASE_URL = "mongodb+srv://myg1742:hBNg2NZ8bblnzzpu@cluster0.jplwuqg.mongodb.net/tms?retryWrites=true&w=majority"
+    static DATABASE_URL = "mongodb+srv://dhingratushar825:5wxIfbGbJlEYrMwX@cluster0.pkwhhii.mongodb.net/tms?retryWrites=true&w=majority"
 }
